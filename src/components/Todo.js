9d6456d49d90cd2aadfd024f4ace9a4d0d3a5779
ROVIN K SUDHAKARAN <rovinksudhakaran@gmail.com>
2025-03-02 15:22:32 +0530

    Add delete, edit and complete buttons with functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,32 +1,86 @@
 import React from 'react'
 import './Todo.css'
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import { MdDelete } from 'react-icons/md'
+import { IoMdDoneAll } from 'react-icons/io'
+import { FiEdit } from 'react-icons/fi'
 
 function Todo() {
-  const [todo , setTodo] = useState('')
-  const [todos , setTodos] = useState([])
+  const [todo, setTodo] = useState('')
+  const [todos, setTodos] = useState([]) 
+  const [editId, setEditId] = useState(0)
 
-  const handleSubmit = (e)=>{
-      e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
   }
 
-  const addTodo = ()=>{
-    setTodos([...todos,todo])
-    console.log(todos)
+  const addTodo = () => {
+    if (todo !== ('')){
+      setTodos([...todos, {list: todo, id: Date.now(), status: false }])
+      console.log(todos)
+      setTodo('')
+    }
+    if (editId){
+      const editTodo = todos.find((todo) => todo.id === editId )
+      const updateTodo = todos.map((to) => to.id === editTodo.id
+      ? (to = {id : to.id, list : todo})
+      : (to = {id : to.id, list : to.list}))
+      setTodos(updateTodo)
+      setEditId(0)
+      setTodo('')
+    } 
+  }
+
+  const inputRef = useRef('null')
+  
+  useEffect(() => {
+    inputRef.current.focus();
+
+  })
+
+  const onDelete = (id) => {
+    setTodos(todos.filter((to) => to.id !== id))
+  }
+
+  const onComplete = (id) => {
+    let complete = todos.map((list) => {
+      if (list.id === id) {
+        return ({...list, status: !list.status })
+      }
+      return list
+    })
+    setTodos(complete)
+  }
+
+  const onEdit = (id) =>{
+    const editTodo = todos.find((to)=> to.id === id)
+    setTodo(editTodo.list)
+    setEditId(editTodo.id)
   }
 
   return (
-    <div className='container'>
+    <div className = 'container'>
       <h2>TODO APP</h2>
-      <form className='form-group' onSubmit={handleSubmit}>
-        <input type="text" value={todo} placeholder='Enter your todo' className='form-control' onChange={(event)=>setTodo(event.target.value)}/>
-        <button onClick={addTodo}>ADD</button>
+      <form className = 'form-group' onSubmit = {handleSubmit}>
+        <input type = "text" value = {todo} ref = {inputRef} placeholder = 'Enter your todo' className = 'form-control'
+          onChange = {(event) => setTodo(event.target.value)} />
+        <button onClick = {addTodo}> {editId ? 'EDIT' : 'ADD'} </button>
       </form>
-      <div className='list'>
+      <div className = 'list'>
         <ul>
           {
-            todos.map((to)=>(
-              <li>{to}</li>
+            todos.map((to) => (
+              <li className = "list-items" key = {to.id}>
+                <div className = "list-item-list" id = {to.status ? 'list-items' : ''}>{to.list}</div>
+                <span>
+                  <IoMdDoneAll className = 'list-item-icons' id = 'complete' title = 'Complete'
+                    onClick = {() => onComplete(to.id)} />
+                  <FiEdit className = 'list-item-icons' id = 'edit' title = 'Edit' 
+                    onClick = {() => onEdit(to.id)} />
+                  <MdDelete className = 'list-item-icons' id = 'delete' title = 'Delete'
+                    onClick = {() => onDelete(to.id)} />
+                </span>
+              </li>
             ))
           }
         </ul>
